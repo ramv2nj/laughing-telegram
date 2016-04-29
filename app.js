c6dd7ca2542f4bf37c819e68a1e4f3268574c558
Ramesh Chandran <Ramesh Chandran>
2016-04-29 16:13:08 -0400

    new file:   Controllers/bookController.js 	new file:   app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// JavaScript source code
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+var db;   
+
+if(process.env.ENV == 'Test')
+	db = mongoose.connect('mongodb://localhost/bookAPI_test');
+else{
+	db = mongoose.connect('mongodb://localhost/bookAPI');
+}
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+bookRouter = require('./Routes/bookRoutes')(Book);
+app.use('/api/books', bookRouter);
+//app.use('/api/authors', authorRouter);
+
+
+
+app.get('/', function (req, res) {
+    res.send('welcome');
+});
+
+app.listen(port,function(){
+    console.log('listening for port ' + port);
+});
+
+module.exports = app;
